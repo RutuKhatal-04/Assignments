54cba22bbd5d2bfa9c26b508ca095f3741a64be8
RutuKhatal-04 <rutukhatal13@gmail.com>
2025-01-15 01:11:11 +0530

    study material

--- Assignement 1(a)/src/app.ts
+++ Assignement 1(a)/src/app.ts
@@ -11,9 +11,9 @@ app.use(express.json());
 app.get('/split/:input', (req: { params: { input: any; }; }, res: { json: (arg0: any) => void; }) => {
     const { input } = req.params;
     const result = splitString(input);
-    res.json(result);
+      res.json(result);
 });
 
 app.listen(PORT, () => {
-    console.log("Server started on port " + PORT);
+    console.log("Server Started on port " + PORT);
 });
