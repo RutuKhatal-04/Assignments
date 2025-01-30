4a1bbb51d77961cabc3cd21b5050039bf922addf
RutuKhatal-04 <rutukhatal13@gmail.com>
2025-01-30 15:41:08 +0530

    Add Assignment 5 folder with initial files

--- Assignment 4 (Industrial Project)/src/models/associations.ts
+++ Assignment 4 (Industrial Project)/src/models/associations.ts
@@ -5,37 +5,7 @@ import SOW from './Sow';
 import paymentPlan from './paymentPlan';
 import Invoice from './Invoice';
 import lineItem from './lineItems';
-// import { PaymentPlan } from '../controllers/Customer_get';
 
-// // Define associations
-// Organization.belongsToMany(Customer, {
-//   through:CustOrgA,
-  
-  
-  
-// });
-// Customer.belongsToMany(Organization, {
-//   through:CustOrgA,
- 
-// });
-
-// CustOrgA.hasOne(SOW);
-// SOW.belongsTo(CustOrgA,{foreignKey:'CustOrgAId'});
-
-
-// SOW.hasMany(paymentPlan);
-// paymentPlan.belongsTo(SOW, { foreignKey: 'SOWId' });
-
-
-
-// CustOrgA.belongsTo(Organization, { foreignKey: 'OrganizationId' });
-// CustOrgA.belongsTo(Customer, { foreignKey: 'CustomerId' });
-// CustOrgA.hasOne(SOW, { foreignKey: 'CustOrgAId' });
-
-
-
-
-// Define associations
 Organization.belongsToMany(Customer, {
   through: CustOrgA,
 });
